c8aca2cd4f70436707166d469a6b6368e8fdfc55
eren23 <erenak1996@gmail.com>
2020-08-25 23:43:12 +0300

    Categorization added backend warnings to be solved

--- client/src/components/applicants/Applicants.js
+++ client/src/components/applicants/Applicants.js
@@ -23,34 +23,144 @@ const Applicants = ({ getUsers, post: { posts, loading } }) => {
   useEffect(() => {
     getUsers();
   }, [getUsers]);
+
+  const uniq = [...new Set(posts.map((post) => post.status))];
+  console.log(uniq);
   return (
     <div>
-      {posts.map((status) => {
-        if (status.status === "Appointment_Set") {
+      {uniq.map((status) => {
+        if (status === "Appointment_Set") {
+          const filtered = posts.filter(
+            (filt) => filt.status === "Appointment_Set"
+          );
           return (
-            <CardDeck>
-              {loading === true ? (
-                <Loader
-                  type="Puff"
-                  color="#00BFFF"
-                  height={100}
-                  width={100}
-                  timeout={3000} //3 secs
-                />
-              ) : (
-                <ApplicantCard
-                  key={status._id}
-                  name={status.name}
-                  surname={status.surname}
-                  email={status.email}
-                  bid={status.bid}
-                  status={status.status}
-                />
-              )}
-            </CardDeck>
+            <div>
+              <h1>
+                Appointment_Set <br />
+              </h1>
+              <CardDeck>
+                {filtered.map((status) => {
+                  {
+                    return loading === true ? (
+                      <Loader
+                        type="Puff"
+                        color="#00BFFF"
+                        height={100}
+                        width={100}
+                        timeout={3000} //3 secs
+                      />
+                    ) : (
+                      <ApplicantCard
+                        key={status._id}
+                        name={status.name}
+                        surname={status.surname}
+                        email={status.email}
+                        bid={status.bid}
+                        status={status.status}
+                      />
+                    );
+                  }
+                })}
+              </CardDeck>
+            </div>
+          );
+        } else if (status === "Propety_Viewed") {
+          const filtered = posts.filter(
+            (filt) => filt.status === "Propety_Viewed"
+          );
+          return (
+            <div>
+              <h1>Propety_Viewed</h1>
+              <CardDeck>
+                {filtered.map((status) => {
+                  {
+                    return loading === true ? (
+                      <Loader
+                        type="Puff"
+                        color="#00BFFF"
+                        height={100}
+                        width={100}
+                        timeout={3000} //3 secs
+                      />
+                    ) : (
+                      <ApplicantCard
+                        key={status._id}
+                        name={status.name}
+                        surname={status.surname}
+                        email={status.email}
+                        bid={status.bid}
+                        status={status.status}
+                      />
+                    );
+                  }
+                })}
+              </CardDeck>
+            </div>
+          );
+        } else if (status === "Interested") {
+          const filtered = posts.filter((filt) => filt.status === "Interested");
+          return (
+            <div>
+              <h1>Interested</h1>
+              <CardDeck>
+                {filtered.map((status) => {
+                  {
+                    return loading === true ? (
+                      <Loader
+                        type="Puff"
+                        color="#00BFFF"
+                        height={100}
+                        width={100}
+                        timeout={3000} //3 secs
+                      />
+                    ) : (
+                      <ApplicantCard
+                        key={status._id}
+                        name={status.name}
+                        surname={status.surname}
+                        email={status.email}
+                        bid={status.bid}
+                        status={status.status}
+                      />
+                    );
+                  }
+                })}
+              </CardDeck>
+            </div>
           );
         } else {
-          return <p key={status._id}>Empty Card</p>;
+          const filtered = posts.filter(
+            (filt) => filt.status === "Offer_Accepted"
+          );
+          return (
+            <div>
+              <h1>Offer_Accepted</h1>
+              <CardDeck>
+                {filtered.map((status) => {
+                  {
+                    return loading === true ? (
+                      <Loader
+                        type="Puff"
+                        color="#00BFFF"
+                        height={100}
+                        width={100}
+                        timeout={3000} //3 secs
+                      />
+                    ) : (
+                      <ApplicantCard
+                        key={status._id}
+                        name={status.name}
+                        surname={status.surname}
+                        email={status.email}
+                        bid={status.bid}
+                        status={status.status}
+                      />
+                    );
+                  }
+                })}
+              </CardDeck>
+            </div>
+          );
         }
       })}
     </div>
